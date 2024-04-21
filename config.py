f73f35f5e3a2fac96e00794892f015d351c90c91
KingRaistar <166215350+KingRaistar@users.noreply.github.com>
2024-04-22 01:51:22 +0530

    Error done

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ TUT_VID = os.environ.get("TUT_VID","2042880811/3")
 
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", ""))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001709167326"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
